56acada90b83f3708170d386b82782954337c4ae
Saujal Karki <saujalkarki04@gmail.com>
2023-10-21 22:31:05 +0545

    updated learning on AJAX and callback hell

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -5,7 +5,28 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-function getCountryData(country) {
+let lang;
+let cur;
+let neighbours;
+
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}" >
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data['population'] / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${lang}</p>
+    <p class="country__row"><span>💰</span>${cur}</p>
+  </div>
+</article>`;
+  countriesContainer.insertAdjacentHTML('beforeEnd', html);
+  countriesContainer.style.opacity = 1;
+};
+
+function getCountryAndNeighbourData(country) {
   // old school ways of AJAX call
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
@@ -13,8 +34,7 @@ function getCountryData(country) {
 
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
-    let lang;
-    let cur;
+    console.log(data);
 
     for (let key in data.languages) {
       lang = data.languages[key];
@@ -22,24 +42,22 @@ function getCountryData(country) {
     for (let key in data.currencies) {
       cur = data.currencies[key].name;
     }
+    renderCountry(data);
 
-    const html = `<article class="country">
-          <img class="country__img" src="${data.flags.png}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data['population'] / 1000000
-            ).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${lang}</p>
-            <p class="country__row"><span>💰</span>${cur}</p>
-          </div>
-        </article>`;
-    countriesContainer.insertAdjacentHTML('beforeEnd', html);
-    countriesContainer.style.opacity = 1;
+    // getting neighbour country
+    const [neighbour] = data.borders;
+    if (!neighbour) return;
+    console.log(neighbour);
+
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      renderCountry(data2, 'neighbour');
+    });
   });
 }
 
-getCountryData('China');
-getCountryData('Nepal');
-getCountryData('Republic of India');
+getCountryAndNeighbourData('israel');
